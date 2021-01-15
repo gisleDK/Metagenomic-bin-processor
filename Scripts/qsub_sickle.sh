2b3ca03bc13fe85222c937a80a0ab5c87d302835
Gisle Vestergaard <gisves@dtu.dk>
2021-01-15 15:42:31 +0100

    Add files via upload

---
+++ Scripts/qsub_sickle.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+### Note: No commands may be executed until after the #PBS lines
+### Account information
+#PBS -W group_list=<Your group> -A <Your group>
+### Job name (comment out the next line to get the name of the script used as the job name)
+#PBS -N sickle
+### Output files (comment out the next 2 lines to get the job name used instead)
+#PBS -e $PBS_JOBID.sickle.stderr
+#PBS -o $PBS_JOBID.sickle.stdout
+### Only send mail when job is aborted or terminates abnormally
+#PBS -m n
+### Number of nodes
+#PBS -l nodes=1:ppn=38:thinnode
+### Memory
+#PBS -l mem=180gb
+### Requesting time - format is <days>:<hours>:<minutes>:<seconds> (here, 1 hour)
+#PBS -l walltime=01:00:00
+# V1.00 Written by Gisle Vestergaard (gislevestergaard@gmail.com)
+# Go to the directory from where the job was submitted (initial directory is $HOME)
+echo Working directory is "$PBS_O_WORKDIR"
+cd "$PBS_O_WORKDIR"
+# This is where the work is done
+# This script is used in Step 1 of the binning workflow which removes adapter sequence.
+# Remember to specify your adapters with literal=Adapter1,Adapter2... or use reference adapters with ref=adapters.fa
+# Remember to adjust forcetrimleft=3 according to your FASTQC reports
+[ $# != 3 ] && { echo "Usage: qsub -F '<R1 FILE> <R2 FILE> <OUTPUT DIR>' qsub_sickle"; exit 1; }
+
+R1=$1
+R1_pure=$(echo "$R1" | rev | cut -f1 -d'/' | rev)
+R2=$2
+R2_pure=$(echo "$R2" | rev | cut -f1 -d'/' | rev)
+OUTDIR=$3
+mkdir -p "$OUTDIR"
+/home/projects/<Your group>/data/Bin/Programs/sickle-1.33/sickle pe -g --qual-type sanger -f "$R1" -r "$R2" -o "$OUTDIR"/"$R1_pure" -p "$OUTDIR"/"$R2_pure" -s "$OUTDIR"/"$R1_pure.singles" -l 100
