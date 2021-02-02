870d8b27364fd025beed248428e91e9b1002daf7
Gisle Vestergaard <gisves@dtu.dk>
2021-02-02 16:24:58 +0100

    Create fasta_select_file_size.r

---
+++ Bin/fasta_select_file_size.r
@@ -0,0 +1,22 @@
+## ---------------------------
+##
+## Script name: biopieces_fasta_select_file_size.r
+##
+## Purpose of script: Create a plot of all bin sizes
+##
+## Part of Biopythonpieces
+##
+## Author: Gisle Vestergaard
+##
+## Date Created: 02-02-2021
+##
+## Email: gislevestergaard@gmail.com
+##
+## ---------------------------
+## load up the packages we will need:
+require(tidyverse)
+## ---------------------------
+## setwd("?")
+bins <- read_delim("./data/raw/binsize.tsv", "\t", col_names = FALSE)
+ggplot(data=bins, aes(X2)) + geom_histogram(binwidth = 1000) + xlab("Bin size in bp") + coord_cartesian(ylim=c(0,20), xlim=c(0,10000000)) + theme_bw()
+ggsave("plot_binsizes.png", width = 5, height = 5)
