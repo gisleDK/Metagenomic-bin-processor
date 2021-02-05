f7093b22a6194d149a602027578a64d0cf54e9fe
Gisle Vestergaard <gisves@dtu.dk>
2021-02-05 15:30:05 +0100

    Update fasta_select_file_size.r

--- Bin/fasta_select_file_size.r
+++ Bin/fasta_select_file_size.r
@@ -1,3 +1,4 @@
+#!/usr/bin/env Rscript
 ## ---------------------------
 ##
 ## Script name: biopieces_fasta_select_file_size.r
@@ -19,4 +20,6 @@ require(tidyverse)
 ## setwd("?")
 bins <- read_delim("./data/raw/binsize.tsv", "\t", col_names = FALSE)
 ggplot(data=bins, aes(X2)) + geom_histogram(binwidth = 1000) + xlab("Bin size in bp") + coord_cartesian(ylim=c(0,20), xlim=c(0,10000000)) + theme_bw()
-ggsave("plot_binsizes.png", width = 5, height = 5)
+ggsave("plot_binsizes.png")
+file.exists("Rplots.pdf")
+file.remove("Rplots.pdf")
