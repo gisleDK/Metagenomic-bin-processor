e87db8f5bbb60d9298d0562162254f8021c998c6
Trine Zachariasen <trizac@g-12-l0002.cm.cluster>
2021-02-11 15:31:05 +0100

    Updating existing files

--- gene_count_matrix/run_snake.sh
+++ gene_count_matrix/run_snake.sh
@@ -8,7 +8,7 @@
 #PBS -e snakemake.err
 #PBS -o snakemake.log
 ### Number of nodes
-#PBS -l nodes=1:ppn=40:thinnode,walltime=20:00:00
+#PBS -l nodes=1:ppn=40:thinnode,walltime=24:30:00
 ### Add current shell environment to job (comment out if not needed)
 #PBS -V
 #PBS -d /home/projects/cu_10108/people/trizac/VAMB_genes
@@ -16,4 +16,5 @@
 
 source /home/projects/cu_10108/people/trizac/bash_modules
 
-snakemake --snakefile Snakefile_VAMBgenes --resources  mem_gb=1900 --cluster 'qsub -W group_list=cu_10108 -A cu_10108 -l nodes=1:ppn={threads},mem={resources.mem_gb}gb,walltime={resources.runtime} -V -d /home/projects/cu_10108/people/trizac/VAMB_genes' --directory /home/projects/cu_10108/people/trizac/VAMB_genes --jobs 100 --jobname "snake.{name}.{jobid}" --printshellcmds --latency-wait 20
+snakemake --snakefile /home/projects/cu_10108/people/trizac/Metagenomic-bin-processor/gene_count_matrix/Snakefile_VAMBgenes --resources  mem_gb=1900 --cluster 'qsub -W group_list=cu_10108 -A cu_10108 -l nodes=1:ppn={threads},mem={resources.mem_gb}gb,walltime={resources.runtime} -V -d /home/projects/cu_10108/people/trizac' --directory /home/projects/cu_10108/people/trizac --jobs 100 --jobname "snake.{name}.{jobid}" --printshellcmds --latency-wait 200
+
