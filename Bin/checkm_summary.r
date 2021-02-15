5390fcc41140b136d57222516f10fbb26078875f
gisleDK <gisves@dtu.dk>
2021-02-15 11:39:46 +0100

    New script for showing bin quality

---
+++ Bin/checkm_summary.r
@@ -0,0 +1,29 @@
+## ---------------------------
+##
+## Script name: checkm_summary.r
+##
+## Purpose of script: Create a plot of bin quality
+##
+## Part of metagenomic-bin-processor
+##
+## Author: Gisle Vestergaard
+##
+## Date Created: 10-02-2021
+##
+## Email: gislevestergaard@gmail.com
+##
+## ---------------------------
+## load up the packages we will need:
+require(tidyverse)
+## ---------------------------
+## setwd("?")
+args = commandArgs(trailingOnly=TRUE)
+bins <- readr::read_tsv(args[1], col_names = TRUE)
+bins_rounded <- bins %>% mutate_if(is.numeric, round)
+bins_rounded$Quality <- "<10% contamination"
+bins_rounded$Quality[bins$Contamination < 10] <- ">10% contamination"
+bins_rounded$Quality[bins$Contamination < 5] <- ">5% contamination"
+bins_rounded$Quality[bins$Contamination < 1] <- ">1% contamination"
+bins_rounded$Quality <- factor(bins_rounded$Quality, levels = c(">10% contamination", "<10% contamination", ">5% contamination", ">1% contamination"))
+ggplot(bins_rounded, aes(x = Completeness, fill = Quality)) + geom_area(binwidth = 1, stat = "bin")
+ggsave("plot_binsizes.png", width = 5, height = 5)
