3164ab8fa07e977bc4cd41a617eff98420e8f4c5
Trine Zachariasen <trizac@g-12-l0002.cm.cluster>
2021-02-16 09:28:26 +0100

    Updating existing files

--- gene_count_matrix/run_snake.sh
+++ gene_count_matrix/run_snake.sh
@@ -8,7 +8,7 @@
 #PBS -e snakemake.err
 #PBS -o snakemake.log
 ### Number of nodes
-#PBS -l nodes=1:ppn=40:thinnode,walltime=24:30:00
+#PBS -l nodes=1:ppn=40:thinnode,walltime=72:30:00
 ### Add current shell environment to job (comment out if not needed)
 #PBS -V
 #PBS -d /home/projects/cu_10108/people/trizac/VAMB_genes
@@ -16,5 +16,7 @@
 
 source /home/projects/cu_10108/people/trizac/bash_modules
 
-snakemake --snakefile /home/projects/cu_10108/people/trizac/Metagenomic-bin-processor/gene_count_matrix/Snakefile_VAMBgenes --resources  mem_gb=1900 --cluster 'qsub -W group_list=cu_10108 -A cu_10108 -l nodes=1:ppn={threads},mem={resources.mem_gb}gb,walltime={resources.runtime} -V -d /home/projects/cu_10108/people/trizac' --directory /home/projects/cu_10108/people/trizac --jobs 100 --jobname "snake.{name}.{jobid}" --printshellcmds --latency-wait 200
+#snakemake --snakefile /home/projects/cu_10108/people/trizac/Metagenomic-bin-processor/gene_count_matrix/Snakefile_VAMBgenes --resources  mem_gb=1900 --cluster 'qsub -W group_list=cu_10108 -A cu_10108 -l nodes=1:ppn={threads},mem={resources.mem_gb}gb,walltime={resources.runtime} -V -d /home/projects/cu_10108/people/trizac' --directory /home/projects/cu_10108/people/trizac --jobs 100 --jobname "snake.{name}.{jobid}" --printshellcmds --latency-wait 200
 
+# Forcing rerun of rules with nuc instead of prot
+snakemake --snakefile /home/projects/cu_10108/people/trizac/Metagenomic-bin-processor/gene_count_matrix/Snakefile_VAMBgenes --resources  mem_gb=1900 --cluster 'qsub -W group_list=cu_10108 -A cu_10108 -l nodes=1:ppn={threads},mem={resources.mem_gb}gb,walltime={resources.runtime} -V -d /home/projects/cu_10108/people/trizac' --directory /home/projects/cu_10108/people/trizac --jobs 100 --jobname "snake.{name}.{jobid}" --printshellcmds --latency-wait 200 -R nonredundant_catalogue minimap_index minimap_align count_genes gene_names create_header gene_count_matrix gene_lists all
